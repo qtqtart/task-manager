96f6bb939f94b7d5f90f12bbbe91b18c88467fbd
artem merkulov <qtqt.art@gmail.com>
2024-09-21 14:53:14 +0300

    feat: init task-manager-server

---
+++ task-manager-server/eslint.config.mjs
@@ -0,0 +1,28 @@
+import js from "@eslint/js";
+import perfectionist from "eslint-plugin-perfectionist";
+import prettier from "eslint-plugin-prettier";
+import globals from "globals";
+import ts from "typescript-eslint";
+
+const { config, plugin } = ts;
+
+export default config(
+  ...ts.configs.recommended,
+  js.configs.recommended,
+  {
+    ignores: ["dist", "node_modules"],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.es2024,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": plugin,
+      prettier,
+    },
+  },
+  perfectionist.configs["recommended-alphabetical"],
+);
